fa325fc51adee9b2f5e3630d7b64bc206c1dc11a
unknown <180878@internet.com>
2022-01-22 15:21:04 +0800

    first commit

--- eladmin-system/src/main/java/me/zhengjie/modules/security/rest/AuthorizationController.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/security/rest/AuthorizationController.java
@@ -30,11 +30,11 @@ import me.zhengjie.modules.security.config.bean.LoginCodeEnum;
 import me.zhengjie.modules.security.config.bean.LoginProperties;
 import me.zhengjie.modules.security.config.bean.SecurityProperties;
 import me.zhengjie.modules.security.security.TokenProvider;
+import me.zhengjie.modules.security.service.OnlineUserService;
 import me.zhengjie.modules.security.service.dto.AuthUserDto;
 import me.zhengjie.modules.security.service.dto.JwtUserDto;
-import me.zhengjie.modules.security.service.OnlineUserService;
-import me.zhengjie.utils.RsaUtils;
 import me.zhengjie.utils.RedisUtils;
+import me.zhengjie.utils.RsaUtils;
 import me.zhengjie.utils.SecurityUtils;
 import me.zhengjie.utils.StringUtils;
 import org.springframework.http.HttpStatus;
@@ -44,7 +44,11 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
@@ -73,18 +77,21 @@ public class AuthorizationController {
     @ApiOperation("登录授权")
     @AnonymousPostMapping(value = "/login")
     public ResponseEntity<Object> login(@Validated @RequestBody AuthUserDto authUser, HttpServletRequest request) throws Exception {
-        // 密码解密
+        // 密码解密,通过私钥（已配置好）解密
         String password = RsaUtils.decryptByPrivateKey(RsaProperties.privateKey, authUser.getPassword());
         // 查询验证码
         String code = (String) redisUtils.get(authUser.getUuid());
         // 清除验证码
         redisUtils.del(authUser.getUuid());
+        //登录时，首先查验，验证码是否过期or不存在
         if (StringUtils.isBlank(code)) {
             throw new BadRequestException("验证码不存在或已过期");
         }
+        //前端输入验证码是否为空 || 验证码是否正确，即对比redis存的验证码信息
         if (StringUtils.isBlank(authUser.getCode()) || !authUser.getCode().equalsIgnoreCase(code)) {
             throw new BadRequestException("验证码错误");
         }
+//        spring-security是怎么做到耦合用户信息的？？？
         UsernamePasswordAuthenticationToken authenticationToken =
                 new UsernamePasswordAuthenticationToken(authUser.getUsername(), password);
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
@@ -94,6 +101,7 @@ public class AuthorizationController {
         // Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
         // SecurityContextHolder.getContext().setAuthentication(authentication);
         String token = tokenProvider.createToken(authentication);
+//        这一步做了什么，将用户信息全部导入jwtUserDto？？？
         final JwtUserDto jwtUserDto = (JwtUserDto) authentication.getPrincipal();
         // 保存在线信息
         onlineUserService.save(jwtUserDto, token, request);
@@ -119,16 +127,37 @@ public class AuthorizationController {
     @AnonymousGetMapping(value = "/code")
     public ResponseEntity<Object> getCode() {
         // 获取运算的结果
+/*        手动修改验证码类型
+        LoginCode loginCode = new LoginCode();
+        loginCode.setCodeType(LoginCodeEnum.gif);
+        loginProperties.setLoginCode(loginCode);
+*/
         Captcha captcha = loginProperties.getCaptcha();
         String uuid = properties.getCodeKey() + IdUtil.simpleUUID();
+        System.out.println("uuid: "+uuid);
         //当验证码类型为 arithmetic时且长度 >= 2 时，captcha.text()的结果有几率为浮点型
+//        captchaValue就是验证码对应的答案
         String captchaValue = captcha.text();
+        System.out.println("验证码对应的答案: "+captchaValue);
+        System.out.println("参数类型： "+captcha.getCharType());
+        System.out.println("算术相关： "+LoginCodeEnum.arithmetic.ordinal());
         if (captcha.getCharType() - 1 == LoginCodeEnum.arithmetic.ordinal() && captchaValue.contains(".")) {
             captchaValue = captchaValue.split("\\.")[0];
         }
         // 保存
-        redisUtils.set(uuid, captchaValue, loginProperties.getLoginCode().getExpiration(), TimeUnit.MINUTES);
-        // 验证码信息
+//        将uuid设置为key,验证码的值设置为value，验证码有效期，已设置为2min，TimeUnit.MINUTES就是将时间单位设置为分钟
+//        LoginCode---private Long expiration = 2L;
+        Long expiration = loginProperties.getLoginCode().getExpiration();
+        System.out.println("验证码有效期，已设置为2min "+expiration);
+//        System.out.println("过期时间 "+ TimeUnit.MINUTES.toMinutes(1000));
+//        TimeUnit timeUnit = TimeUnit.HOURS;
+//        System.out.println("一天有多少小时："+timeUnit.convert(1l, TimeUnit.DAYS));
+//        timeUnit = TimeUnit.MILLISECONDS;
+//        System.out.println("一天有多少毫秒："+timeUnit.convert(1l, TimeUnit.DAYS));
+//        timeUnit = TimeUnit.MINUTES;
+//        System.out.println("一周有多少分钟："+timeUnit.convert(7l, TimeUnit.DAYS));
+        redisUtils.set(uuid, captchaValue, expiration, TimeUnit.MINUTES);
+        // 给前端的验证码信息
         Map<String, Object> imgResult = new HashMap<String, Object>(2) {{
             put("img", captcha.toBase64());
             put("uuid", uuid);
